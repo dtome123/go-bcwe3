9b3af2f51796bf1492146a291aadc95950ed6612
phtom <phtom1999@gmail.com>
2025-03-31 16:47:28 +0700

    update

---
+++ eth/main.go
@@ -0,0 +1,35 @@
+package eth
+
+import (
+	"context"
+	"log"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+type Ethereum struct {
+	Client *ethclient.Client
+}
+
+func NewEthereum() *Ethereum {
+	client, err := ethclient.Dial("http://localhost:8545")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &Ethereum{
+		Client: client,
+	}
+}
+
+func (e *Ethereum) Close() {
+	e.Client.Close()
+}
+
+func (e *Ethereum) GetCurrentBlock() uint64 {
+	blockNumber, err := e.Client.BlockNumber(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	return blockNumber
+}
